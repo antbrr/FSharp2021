47abd707dba0096e7281bd6135caefe78856b1d4
antbrr <antbr@itu.dk>
2022-07-17 19:12:00 +0200

    opg 2 stuff

--- Exam2021jylland/Program.fs
+++ Exam2021jylland/Program.fs
@@ -66,5 +66,94 @@ let path2 p (ms: move list) =
 
             
 let path3 _ = failwith "not implemented"
-        
+
+
+
+
+//OPG 2
+
+let foo f =
+ let mutable m = Map.empty
+ let aux x =
+     match Map.tryFind x m with
+     | Some y when Map.containsKey x m -> y
+     | None ->
+     m <- Map.add x (f x) m; f x
+ aux
+let rec bar x =
+ match x with
+ | 0 -> 0
+ | 1 -> 1
+ | y -> baz (y - 1) + baz (y - 2)
+
+and baz = foo bar
+
+
+(* 2.1
+
+ Q : What are the types of functions foo , bar , and baz ?
+
+ A:
+ foo: ('a -> 'b) -> 'a -> 'b
+ bar: int -> int
+ baz: int -> int
+ 
+ Q : What do functions foo and baz do (skip bar )? Focus on what they do rather than how they do it.
+ 
+ A: 
+ 
+ foo applies function to element. It acts as a mapping function.
+ 
+ baz takes an integer and outputs the fibonacci number of the integer input.
+ 
+ 
+ 
+ 
+ 
+ Q: The function foo uses a mutable variable.
+What function does it serve (why is it there)?
+What would happen if you removed the mutable keyword from the line let mutable m = Map.empty ?
+Would the function foo still work? If yes, why; if no, why not?
+What would be appropriate names for functions foo , bar , and baz ?
+ 
+ A: It is there because the variable needs to be reassigned/updated on line 81, and if we removed it, that line of code would not compile.
+ 
+ A: Appropriate names would be foo: Map; bar: Fibonacci, baz: Fibonacci2
+ 
+ 
+ 
+ 
+ *)
+ 
+ (* 2.2
+ 
+ It is used to define mutually recursive functions (functions that calls eachother)
+ 
+ This line: y -> baz (y - 1) + baz (y - 2) does not compile because bar cant find baz now due to the fact that baz is defined later in the code (if we remove and)
+ 
+ 
+ 
+ *)
+ 
+ 
+let foo2 f =
+ let mutable m = Map.empty
+ let aux x =
+     match Map.tryFind x m with
+     | Some y -> y
+     | None ->
+     let y = f x
+     m <- Map.add x y m; y
+ aux
+
+let bazSeq = Seq.initInfinite baz
+
+
+let rec barbaz x =
+ let baz = foo barbaz
+ match x with
+ | 0 -> 0
+ | 1 -> 1
+ | y -> baz (y - 1) + baz (y - 2)        
+  
         
