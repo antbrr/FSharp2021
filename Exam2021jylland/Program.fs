4180091a304b9e142365719cdc96f7a7877dc34f
antbrr <antbr@itu.dk>
2022-07-28 19:42:07 +0200

    Opg1 updated with continuation

--- Exam2021jylland/Program.fs
+++ Exam2021jylland/Program.fs
@@ -92,33 +92,23 @@ C(0,0) :: C(0, -5) :: C(10,-5)
 
 [C(0,0); C(0,-5); C(10,-5)];
 
+So as we can see, path is not tail recursive because it constructs the list at the very end, and not incrementally like an accumulator
 
-
-
-
-
-let rec path (P(c,_) as p)(ms: move list) =
-    match ms with
-    | [] -> [c]
-    | Forward x :: xs -> c :: (path (step p (Forward x)) xs)
-    | x :: xs -> path (step p x) xs
-
-let move (dist: int) (dir: direction) (C(x,y): coord) =
-    match dir with
-    |North -> C(x,y-dist)
-    |South -> C(x,y+dist)
-    |West -> C(x-dist,y)
-    |East -> C(x+dist,y)
-
-    
-    
 *)
         
 
 //path3 tbd
 
             
-let path3 _ = failwith "not implemented"
+let path3 (p: position) (ms: move list) =
+    let rec aux cont (P(c,_) as p) ms' =
+        match ms' with
+        | [] -> cont [c]
+        | Forward x :: xs ->
+            let p' = step p (Forward x)
+            aux (fun result -> cont(c :: result)) p' xs
+        | x :: xs -> aux cont (step p x) xs
+    aux id p ms
 
 
 
@@ -164,14 +154,31 @@ and baz = foo bar
  
  
  Q: The function foo uses a mutable variable.
-What function does it serve (why is it there)?
+ Q: What function does it serve (why is it there)?
+ A: It is there because the variable needs to be reassigned/updated on line 81.
+
 What would happen if you removed the mutable keyword from the line let mutable m = Map.empty ?
 Would the function foo still work? If yes, why; if no, why not?
-What would be appropriate names for functions foo , bar , and baz ?
+
+ If we removed it, the line of code would not compile. Because you cannot update an immutable variable.
+
+ Q: What would be appropriate names for functions foo , bar , and baz ?
  
- A: It is there because the variable needs to be reassigned/updated on line 81, and if we removed it, that line of code would not compile.
+ A: Appropriate names would be:
  
- A: Appropriate names would be foo: Map; bar: Fibonacci, baz: Fibonacci2
+ foo: foo could be called memoize (it does memoization), cache, remember, or something similar. 
+ 
+ bar: fib
+ 
+ baz: fibAux
+ 
+ Note: A very common problem for this assignment was the confusion with what a function does with how it does it.
+    The mutable state is squarely in how something is done not what is actually being computed, and the baz function in particular had a lot of creative suggestions for how it did things rather than what it did.
+    All it does is compute fibonacci numbers. If I gave you baz as a black box you could not infer anything else from it. Your safest bet would most likely be that it was a tail recursive fibonacci function since it is fast.
+    Keep this in mind. The distinction is important.
+
+
+    
  
  
  
@@ -180,14 +187,41 @@ What would be appropriate names for functions foo , bar , and baz ?
  
  (* 2.2
  
- It is used to define mutually recursive functions (functions that calls eachother)
+ Q: What function does this keyword serve in general (why would you use and when writing any program)?
  
- This line: y -> baz (y - 1) + baz (y - 2) does not compile because bar cant find baz now due to the fact that baz is defined later in the code (if we remove and)
+ A: It is used to define mutually recursive functions (functions that calls eachother). We would use it, if this is a property we need to utilize.
  
- 
+ Q: What would happen if you removed it from this particular program and replaced it with a standard let (change
+    the line and baz = foo bar to let baz = foo bar )?
+
+ A: This line: y -> baz (y - 1) + baz (y - 2) does not compile because bar cant find baz now due to the fact that baz is defined later in the code (if we remove and)
  
  *)
  
+ (* 2.3
+ 
+ Q: The function foo generates a warning during compilation: Warning: Incomplete pattern matches on this expression.
+ 
+ Why does this happen, and where?
+ 
+ A: It happens at this line "match Map.tryFind x m with". It happens because we have a "when" guard on the line, 
+ "|Some y when Map.containsKey x m -> y", therefore it gives this error since we dont have a pattern match for the case where Map.containskey returns false.
+ 
+ 
+ Q: For these particular three functions will this incomplete pattern match ever cause problems for any possible
+execution of baz ? If yes, why; if no, why not.
+
+A: It will not cause a problem, because when there is Some y,it is always true that the map contains the x value thus making the when clause redundant.
+
+Q: The function foo has two redundant computations and is hence not as efficient as it could be. What are these
+two computations and why are they redundant?
+
+A: The first redudant computation is the when clause, when Map.containsKey x m, as described above.
+    The second redundant computation is the fx value which is calculated 2 times on the line m <- Map.add x (f x) m; f x
+
+*)
+
+//2.4
  
 let foo2 f =
  let mutable m = Map.empty
@@ -199,6 +233,7 @@ let foo2 f =
      m <- Map.add x y m; y
  aux
 
+// 2.5
 let bazSeq = Seq.initInfinite baz
  
  //Question 3: Guess the next sequence element
